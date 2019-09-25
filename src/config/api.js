b010e6d39df644398c27dab80c82a69ff75f800c
xv1998 <1324841784@qq.com>
2019-09-25 16:52:25 +0800

    登录界面

---
+++ src/config/api.js
@@ -0,0 +1,12 @@
+const syllabus = 'https://syllabus.candycute.cn'
+const api = {
+    getLesson: syllabus + '/user/login',  // 登录
+}
+
+const errCode = {
+    // validateLoginState: {
+    //     no_skey: '01030101'
+    // }
+}
+
+export { api, errCode }
---
+++ src/pages/login/login.js
@@ -0,0 +1,84 @@
+// src/pages/login/login.js
+Page({
+    account: '',
+    password: '',
+    /**
+     * 页面的初始数据
+     */
+    data: {},
+    /**
+     * 绑定账号
+     * @param e
+     */
+    getAccount: function(e){
+        this.account = e.detail.value
+    },
+    /**
+     * 绑定密码
+     * @param e
+     */
+    getPassword: function(e){
+        this.password = e.detail.value
+    },
+    /**
+     * 登录
+     */
+    bindLogin: function(){
+
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
